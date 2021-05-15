7ed41a4229393694b68caa7d0f0d7127b597511d
muto15 <56058496+muto15@users.noreply.github.com>
2021-05-15 17:16:53 +0900

    a

--- docs/grd.js
+++ docs/grd.js
@@ -1,7 +1,7 @@
 var canvas;
 function setup(){
     canvas = createCanvas(640,480);
-    canvas.parent("oekaki");
+    canvas.parent("mask");
     frameRate(10);
 
 }
@@ -9,11 +9,8 @@ function draw(){
     fill(255,255,255,0);
     noStroke();
     rect(0,0,width,height);
-    if(mouseIsPressed){
+    
 	fill(0);
-    }
-    else{
-	fill(255);
-    }
+    
     ellipse(mouseX, mouseY, 80, 80);
 }
