fa75944b9cc2fc652a1d612ddb7c9a86ded5c85f
muto15 <56058496+muto15@users.noreply.github.com>
2021-05-15 18:39:54 +0900

    あああ

--- docs/grd.js
+++ docs/grd.js
@@ -1,16 +1,20 @@
 var canvas;
 function setup(){
-    canvas = createCanvas(640,480);
-    canvas.parent("mask");
+    canvas1 = createCanvas(460,240);
+    canvas1.parent("mainvisual");
+   
+  
     frameRate(10);
 
 }
 function draw(){
-    fill(255,255,255,0);
-    noStroke();
-    rect(0,0,width,height);
     
+    
+	background(255);
+  // fill(255);
+  // rect(0,0,width,height);
 	fill(0);
     
     ellipse(mouseX, mouseY, 80, 80);
+   
 }
