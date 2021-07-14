b2612f1c82016771ddd85ca832bb07143616eb0b
muto15 <sayaka490@ybb.ne.jp>
2021-07-14 17:41:52 +0900

    完成

--- docs/dist/okuyuki.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(window).scroll(function(){
-    var y = $(this).scrollTop();
-    $('#bg01').css('background-position', '0 ' + parseInt( -y / 5 ) + 'px');
-    $('#bg02').css('background-position', '0 ' + parseInt( -y / 10 ) + 'px');
-    $('#bg03').css('background-position', '0 ' + parseInt( -y / 30 ) + 'px');
-    });
-    });
